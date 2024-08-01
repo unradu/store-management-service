77483e2f8746e2521f047bb722aef6418c5802ab
Radu Ungureanu <ungureanu.radu19@gmail.com>
2024-08-01 16:16:09 +0300

    add project skeleton

---
+++ src/main/java/com/demo/store/StoreManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/store/StoreManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
